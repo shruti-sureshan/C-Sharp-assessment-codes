bb40102182e55e3d1b370bdc75bd1a119b6e9726
Shruti Sureshan <67042828+shruti-sureshan@users.noreply.github.com>
2021-06-29 16:21:00 +0530

    Add files via upload

---
+++ Sum.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyApplication
+{
+  class Sum
+  {
+    static void Main(string[] args)
+    {
+      int i = 0;
+      while (i < 10) 
+      {
+        Console.WriteLine(i);
+        i++;
+      }
+    }
+  }
+}
